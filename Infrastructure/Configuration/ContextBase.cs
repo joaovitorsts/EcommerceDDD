beaaae0f4b8a89eeec6520a01f2ddeec4f72df36
João Vitor Santos <jsantos@isolutions.com.br>
2022-02-01 16:27:16 -0300

    Infraestrutura base do projeto

---
+++ Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,33 @@
+﻿using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Produto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConnectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        } 
+
+        private string GetStringConnectionConfig()
+        {
+            string strCon = "Data Source=;Initial Catalog=DDD_ECOMMERCE; Integrated Security=False;User ID=sa; Password=15Dev;";
+            return strCon;
+        }
+    }
+}
---
+++ Infrastructure/Repository/Generics/RepositoryGenerics.cs
@@ -0,0 +1,94 @@
+﻿using Domain.Interfaces.Generics;
+using Infrastructure.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.Generics
+{
+    public class RepositoryGenerics<T> : IGeneric<T>, IDisposable where T : class
+    {
+        private readonly DbContextOptions<ContextBase> _OptionsBuilder;
+        private bool disposedValue;
+
+        public RepositoryGenerics()
+        {
+            _OptionsBuilder = new DbContextOptions<ContextBase>();
+        }
+
+
+        public async Task Add(T Objeto)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                await data.Set<T>().AddAsync(Objeto);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task Update(T Objeto)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                data.Set<T>().Update(Objeto);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task Delete(T Objeto)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                data.Set<T>().Remove(Objeto);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task<T> GetEntityById(int id)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                return await data.Set<T>().FindAsync(id);
+            }
+        }
+
+        public async Task<List<T>> List()
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                return await data.Set<T>().AsNoTracking().ToListAsync();
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~RepositoryGenerics()
+        // {
+        //     // Não altere este código. Coloque o código de limpeza no método 'Dispose(bool disposing)'
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Não altere este código. Coloque o código de limpeza no método 'Dispose(bool disposing)'
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
